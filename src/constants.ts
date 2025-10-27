74b3e8e63f5a3a312c15d7a1406d45a8e006f9bd
juliaoverflow <11952329+Julli4n@users.noreply.github.com>
2025-10-27 07:10:23 -0700

    1761574223586

--- src/constants.ts
+++ src/constants.ts
@@ -5,7 +5,8 @@ export const IPINFO_ACCESS_TOKEN_ENV_KEY = "IPINFO_ACCESS_TOKEN";
 export const PRIVATE_ACCESS_COOKIE_ENV_KEY =
 	"ROBLOX_PRIVATE_ACCESS_SECURITY_TOKEN";
 
-export const ROBLOX_USER_AGENT = "Roblox/WinInet RobloxApp/0";
+export const ROBLOX_USER_AGENT =
+	"Roblox/WinInet RobloxApp/0 (GlbalDist; RobloxDirectDownload)";
 
 export const CITY_TO_NEW_LOCATION: Record<string, DataCenterLocation> = {
 	Secaucus: {
